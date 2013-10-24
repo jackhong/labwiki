5204bc7e20617bc60901758108566dbbb8cc20ea
Jack C Hong <jhong@nicta.com.au>
2013-10-24 17:48:15 +1100

    Add foundation for compass, no to bootstrap
    Needs to use it with requirejs

--- app/template/layout.rb
+++ app/template/layout.rb
@@ -7,12 +7,14 @@ module LW::Template
     depends_on :css, "/css/print.css", media: "print"
 
     def body_content
+      navigation
+
       form(action: "/login", method: "post") {
         h1 "Sign in with OpenID"
 
         img src: "/login/img/geni.png"
 
-        url "Login with GENI ID", id: "geni", "data-toggle" => "button"
+        a "Login with GENI ID", id: "geni", "data-toggle" => "button"
 
         a(href: "http://www.nicta.com.au", target: "blank") {
           img src: "/login/img/Logo-Nicta-S.jpg"
@@ -20,6 +22,21 @@ module LW::Template
 
         a(href: "http://gimi.ecs.umass.edu", target: "blank") {
           img src: "/login/img/logo-gimi.png"
+        }
+      }
+    end
+
+    def navigation
+      nav(class: "top-bar") {
+        ul(class: "title-area") {
+          li.name {
+            h1 { a("Labwiki", href: "#") }
+          }
+        }
+        section(class: "top-bar-section") {
+          ul.right {
+            li { a("About", href: "#") }
+          }
         }
       }
     end
